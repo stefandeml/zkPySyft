a6152c18b85924d71095babe836c9302816c83af
Valentin Ganev <sanchopansa@gmail.com>
2019-03-26 16:24:01 +0200

    add helper scripts

---
+++ clean_zokrates_output.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rm -f out out.code proof.json proving.key variables.inf verification.key witness zokrates.code
---
+++ run_example.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python -m zkPySyft.main example_data/example_pysyft_plan example_data/example_pysyft_input 0
